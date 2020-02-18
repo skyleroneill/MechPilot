e6a98b168e73e3a34ea3b14d8aae65617d6efefd
Ben Przysucha <ben.mozilla@gmail.com>
2020-02-17 19:15:46 -0500

    Ben: working with fixing the floaty jump, its sort of working

--- Assets/DeveloperFolders/Ben/PlayerController.cs
+++ Assets/DeveloperFolders/Ben/PlayerController.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private const float movementSpeed = 10f;
-    private const float jumpPower = 10f;
+    private const float jumpPower = 12.5f; // Tried values: 10, 15, 12.5
+    private float fallMultiplier = 2.5f;
+    private float lowJumpMultiplier = 2f;
     private bool isGrounded;
     private Rigidbody2D rb2d;
-    private BoxCollider2D col2d;
     private RaycastHit2D hit2d;
     
     public Transform feetPosition;
@@ -18,7 +19,6 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        col2d = GetComponent<BoxCollider2D>();
     }
     
     private void Update()
@@ -32,28 +32,26 @@ public class PlayerController : MonoBehaviour
         rb2d.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb2d.velocity.y);
         
         isGrounded = Physics2D.OverlapCircle(feetPosition.position, 0.5f, groundLayer);
-        //hit2d = Physics2D.CircleCast(feetPosition.position, 0.5f, Vector2.down, 0f, groundLayer);
-
-        //print($"Player is on the ground: {isGrounded}");
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !Input.GetKeyDown(KeyCode.S) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && !Input.GetKey(KeyCode.S) && isGrounded)
         {
             rb2d.AddForce(new Vector2(0f, jumpPower), ForceMode2D.Impulse);
             isGrounded = false;
         }
+        
+        rb2d.velocity += Time.deltaTime * Physics2D.gravity.y * (fallMultiplier - 1) * Vector2.up;
     }
 
     private void JumpDown()
     {
-        if (Input.GetKeyDown(KeyCode.K) && isGrounded)
-        //if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.S)) || (Input.GetKeyDown(KeyCode.S) && Input.GetKeyDown(KeyCode.Space)) && isGrounded)
+        if ((Input.GetKeyDown(KeyCode.Space) && Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.Space)) && isGrounded)
         {
             hit2d = Physics2D.CircleCast(feetPosition.position, 0.5f, Vector2.down, 0f, groundLayer);
 
-            if (hit2d.collider.gameObject.layer == 10)
+            if (hit2d.collider.gameObject.layer == groundLayer)
             {
                 StartCoroutine(TemporaryTrigger(hit2d));
             }
