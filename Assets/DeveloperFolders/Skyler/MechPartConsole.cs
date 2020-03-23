c335f1302e54b27d176bf4b9a01133cd81158555
Skyler <one2@umbc.edu>
2020-03-23 13:47:36 -0400

    Added ability to repair mech parts via the consoles

--- Assets/DeveloperFolders/Skyler/MechPartConsole.cs
+++ Assets/DeveloperFolders/Skyler/MechPartConsole.cs
@@ -6,27 +6,35 @@ public class MechPartConsole : MonoBehaviour
 {
     public MechPartController mechPart;
     public KeyCode activationKey;
+    public KeyCode repairKey;
     public GameObject pilot;
     public bool canActivate = true;
     public bool checkPartHealth = true;
+    public float repairCooldown = 0.5f;
+    public int repairRate = 1;
 
     private bool pilotNearby = false;
     private bool activatable = true;
+    private Health partHealth;
+
+    private float repairTimer = 0f;
 
     private void Start()
     {
         // Don't check for health if there is none
         checkPartHealth = mechPart.gameObject.GetComponent<Health>() != null;
+        partHealth = mechPart.gameObject.GetComponent<Health>();
         activatable = canActivate;
     }
 
     private void Update()
     {
-        activatable = checkPartHealth ? CheckPartHealth() : canActivate;
+        activatable = checkPartHealth ? CheckPartAlive() : canActivate;
         ActivatePart();
+        RepairPart();
     }
 
-    private bool CheckPartHealth()
+    private bool CheckPartAlive()
     {
         // Return true if the part is alive, false if dead
         return mechPart.gameObject.GetComponent<Health>().GetHealth() > 0;
@@ -53,6 +61,28 @@ public class MechPartConsole : MonoBehaviour
                 pilot.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
             else
                 pilot.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+    }
+
+    private void RepairPart()
+    {
+        if (checkPartHealth && partHealth.GetHealth() < partHealth.GetMaxHealth())
+        {
+            if (Input.GetKey(repairKey) && pilotNearby && !mechPart.IsActive())
+            {
+                repairTimer += Time.deltaTime;
+            }
+            else
+            {
+                repairTimer = 0f;
+                return;
+            }
+            
+            if(repairTimer >= repairCooldown)
+            {
+                partHealth.Heal(repairRate);
+                repairTimer = 0f;
+            }
         }
     }
 
