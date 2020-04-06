a258f373e0495fc20300bfd4e3253331b64f10c5
Zach Hale <zachchale@gmail.com>
2020-04-06 11:54:22 -0400

    Added Better Camera To Mech

---
+++ Assets/FollowActor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowActor : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    // Start is called before the first frame update
+    [SerializeField]
+    float strength;
+
+    // Update is called once per frame
+
+    private void Awake(){
+        transform.parent = null;
+    }
+
+    void Update(){
+        transform.position = Vector3.Lerp(transform.position, target.position, strength * Time.deltaTime);
+    }
+}
