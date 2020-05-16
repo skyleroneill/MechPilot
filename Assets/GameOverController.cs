c0699fa24022f5fc79980caaf1aa7bccca1a2dbc
Skyler <one2@umbc.edu>
2020-05-16 14:21:22 -0400

    Added ground sprites and misc props to main level, added gameover state, updated tutorial text, balanced weapons

---
+++ Assets/GameOverController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    [SerializeField]
+    private Health[] playerHealths;
+    [SerializeField]
+    private string gameOverMessage = "GAME  OVER\nPRESS  SPACE  TO  RESTART";
+    [SerializeField]
+    private Text gameOverText;
+
+    private bool gameOver = false;
+    private void Update()
+    {
+        if(gameOver && Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            return;
+        }
+
+        foreach(Health health in playerHealths)
+        {
+            if(health.GetHealth() > 0)
+            {
+                // Something is alive still, not game over
+                return;
+            }
+        }
+
+        gameOverText.text = gameOverMessage;
+        gameOver = true;
+        Time.timeScale = 0f;
+    }
+}
