0040cf2c8c43699a2aeeb3681339e7b095f5e52a
Skyler <one2@umbc.edu>
2020-05-17 17:17:42 -0400

    Added main menu art, updated buttons, added credits

---
+++ Assets/ToggleCreditsPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCreditsPanel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject creditsPanel;
+
+    public void ToggleCredits()
+    {
+        if (!creditsPanel)
+            return;
+
+        creditsPanel.active = !creditsPanel.active;
+    }
+}
